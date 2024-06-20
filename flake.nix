f71cad02ad74d9165008befba0bf4f913eed4ba3
Akshett Rai Jindal <jindalakshett@gmail.com>
2024-06-20 10:07:54 +0530

    [flake] add flake with devshell

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Flake for chatter-rs/wa";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, fenix }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    with pkgs;
+    {
+
+      devShells.default = mkShell {
+        name = "chatter-rs-wa-dev";
+
+        nativeBuildInputs = [
+          fenix.packages."${system}".stable.toolchain
+        ];
+      };
+
+    }
+  );
+}
